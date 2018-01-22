f22451d0134bf228ead3b0e2139dced95fad48f6
Nick Basile <nicholas.basile@outlook.com>
2018-01-22 11:59:28 -0600

    Styled register view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,52 +3,44 @@
 @section('content')
 <div class="flex items-center justify-center h-9/10">
     <div class="p-8 bg-white rounded shadow-lg m-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 lg:max-w-md">
-        <h2 class="text-grey-darkest text-xl mb-4 text-center tracking-wide">Login</h2>
-        <div>
-            <form method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
+        <h2 class="text-grey-darkest text-2xl mb-4 text-center tracking-wide">Login</h2>
+        <form method="POST" action="{{ route('login') }}">
+            {{ csrf_field() }}
 
-                <div class="mb-4">
-                    <label for="email" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('email') ? 'text-red-dark' : 'text-grey-darker' }}">E-Mail</label>
-                    <div>
-                        <input id="email" type="email" class="p-2 border w-full text-grey-darkest {{ $errors->has('email') ? ' border-red-dark' : 'border-grey-darkest' }}" name="email" value="{{ old('email') }}" required autofocus>
-                        @if ($errors->has('email'))
-                            <span class="mt-2 block text-sm text-red-dark">
-                                {{ $errors->first('email') }}
-                            </span>
-                        @endif
-                    </div>
-                </div>
+            <div class="mb-4">
+                <label for="email" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('email') ? 'text-red-dark' : 'text-grey-darker' }}">E-Mail</label>
+                <input id="email" type="email" class="p-2 border w-full text-grey-darkest {{ $errors->has('email') ? ' border-red-dark' : 'border-grey-darkest' }}" name="email" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                    <span class="mt-2 block text-sm text-red-dark">
+                        {{ $errors->first('email') }}
+                    </span>
+                @endif
+            </div>
 
-                <div class="mb-4">
-                    <label for="password" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('password') ? 'text-red-dark' : 'text-grey-darker' }}">Password</label>
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="p-2 border w-full text-grey-darkest {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-darkest' }}" name="password" required>
-                        @if ($errors->has('password'))
-                            <span class="mt-2 block text-sm text-red-dark">
-                                {{ $errors->first('password') }}
-                            </span>
-                        @endif
-                    </div>
-                </div>
+            <div class="mb-4">
+                <label for="password" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('password') ? 'text-red-dark' : 'text-grey-darker' }}">Password</label>
+                <input id="password" type="password" class="p-2 border w-full text-grey-darkest {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-darkest' }}" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="mt-2 block text-sm text-red-dark">
+                        {{ $errors->first('password') }}
+                    </span>
+                @endif
+            </div>
 
-                <div>
-                    <label class="flex items-center mb-2 uppercase tracking-wide text-sm text-grey-darker">
-                        <input class="mr-2" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                    </label>
-                </div>
+            <label class="flex items-center mb-2 uppercase tracking-wide text-sm text-grey-darker">
+                <input class="mr-2" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+            </label>
 
-                <button class="block mx-auto my-4 bg-teal hover:bg-teal-dark text-white text-lg rounded px-4 py-2" type="submit">
-                    Login
-                </button>
+            <button class="block mx-auto my-4 bg-teal hover:bg-teal-dark text-white text-lg rounded px-4 py-2" type="submit">
+                Login
+            </button>
 
-                <div class="text-center">
-                    <a href="{{ route('password.request') }}" class="no-underline tracking-wide text-sm text-grey-darkest hover:text-black" >
-                        Forgot Your Password?
-                    </a>
-                </div>
-            </form>
-        </div>
+            <div class="text-center">
+                <a href="{{ route('password.request') }}" class="no-underline tracking-wide text-sm text-grey-darkest hover:text-black" >
+                    Forgot Your Password?
+                </a>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+<div class="flex items-center justify-center h-9/10">
+    <div class="p-8 bg-white rounded shadow-lg m-6 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 lg:max-w-md">
+        <h2 class="text-grey-darkest text-2xl mb-4 text-center tracking-wide">Register</h2>
+        <form method="POST" action="{{ route('register') }}">
+            {{ csrf_field() }}
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="mb-4">
+                <label for="name" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('name') ? 'text-red-dark' : 'text-grey-darker' }}">Name</label>
+                <input id="name" type="text" class="p-2 border w-full text-grey-darkest border-grey-darkest {{ $errors->has('name') ? ' border-red-dark' : 'border-grey-darkest' }}" name="name" value="{{ old('name') }}" required autofocus>
+                @if ($errors->has('name'))
+                    <span class="mt-2 block text-sm text-red-dark">
+                        {{ $errors->first('name') }}
+                    </span>
+                @endif
             </div>
-        </div>
+
+            <div class="mb-4">
+                <label for="email" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('email') ? 'text-red-dark' : 'text-grey-darker' }}">E-Mail</label>
+                <input id="email" type="email" class="p-2 border w-full text-grey-darkest {{ $errors->has('email') ? ' border-red-dark' : 'border-grey-darkest' }}" name="email" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                    <span class="mt-2 block text-sm text-red-dark">
+                        {{ $errors->first('email') }}
+                    </span>
+                @endif
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="block mb-2 uppercase tracking-wide text-sm {{ $errors->has('password') ? 'text-red-dark' : 'text-grey-darker' }}">Password</label>
+                <input id="password" type="password" class="p-2 border w-full text-grey-darkest {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-darkest' }}" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="mt-2 block text-sm text-red-dark">
+                        {{ $errors->first('password') }}
+                    </span>
+                @endif
+            </div>
+
+            <div class="mb-4">
+                <label for="password-confirm" class="flex items-center mb-2 uppercase tracking-wide text-sm text-grey-darker">Confirm Password</label>
+                <input id="password-confirm" type="password" class="p-2 border w-full text-grey-darkest border-grey-darkest" name="password_confirmation" required>
+            </div>
+
+            <button type="submit" class="block mx-auto mt-4 bg-teal hover:bg-teal-dark text-white text-lg rounded px-4 py-2">
+                Register
+            </button>
+        </form>
     </div>
 </div>
 @endsection
